7f31a8a0827f5ba2c8567879170b9e3150e9c9c7
hbrcaninovic <hbrcaninov1@etf.unsa.ba>
2023-12-16 20:40:29 +0100

    Adding new main

---
+++ VVS-projekat/Models/CountReservationResult.cs
@@ -0,0 +1,7 @@
+﻿namespace VVS_projekat.Models
+{
+    public class CountReservationResult
+    {
+        public int numberOfReservations { get; set; }
+    }
+}
---
+++ VVS-projekat/Models/LateActivatedResult.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace VVS_projekat.Models
+{
+    public class LateActivatedResult
+    {
+        public List<Reservation> LateActivatedReservations { get; set; }
+    }
+}
