6230a33ceb4d58be01b83123b495c3e7addc84bb
aprasovic1 <aprasovic1@etf.unsa.ba>
2023-12-17 20:45:57 +0100

    Izvrsena ispravka koda

--- VVS-projekat/Controllers/RecommendationsController.cs
+++ VVS-projekat/Controllers/RecommendationsController.cs
@@ -29,7 +29,8 @@ namespace VVS_projekat.Controllers
                 books = GetBestBooks(allBooks);
             }
             catch (ArgumentException ex) //Metoda RecommendationsController.Index() hvata ArgumentException, dok GetAllBooksAsync() baca ArgumentNullException.
-            {
+            {               
+                //Ako nema knjiga u sistemu napiši na ekran
                 ViewBag.EmptyList = "Nema knjiga u sistemu!";
             }
 
@@ -41,32 +42,33 @@ namespace VVS_projekat.Controllers
             return await _context.Book.ToListAsync();
         }
         //Dodati komentare u kod radi bolje čitljivosti i održavanja koda.
+        
+        
+        //Vraća listu najboljih knjiga u sistemu
         public List<Book> GetBestBooks(List<Book> allBooks)
         {
-            if (allBooks.Count == 0) throw new ArgumentNullException("Prazna lista knjiga");
-
+            if (allBooks.Count == 0) throw new ArgumentException("Prazna lista knjiga");
+            
+            // Izračunaj prosjek ratinga za svaku knjigu
             var booksWithAverageRatings = CalculateAverageRatings(allBooks);
-
+            
+            // Sortiraj knjige po prosječnom ratingu
             var sortedBooks = booksWithAverageRatings.OrderByDescending(b => b.AverageRating)
                     .ToList();
 
-
-            var topCount = (int)(sortedBooks.Count); //U metodi GetBestBooks, potrebno je pomnožiti sortedBooks.Count sa 0.3 kako bi se dobile samo najbolje 30% knjiga.
+            // Izdvoji najbolje knjige
+            var topCount = (int)(sortedBooks.Count*0.3); //U metodi GetBestBooks, potrebno je pomnožiti sortedBooks.Count sa 0.3 kako bi se dobile samo najbolje 30% knjiga.
             var topBooks = sortedBooks.Take(topCount).ToList();
 
-            var books = new List<Book>();
-            foreach (var bookWithRating in topBooks) 
-            {
-                books.Add(bookWithRating.Book);
-
-            }
-
-            return books;
+            //Dio koda treba izdvojiti iz metode GetBestBooks i staviti u zasebnu metodu GetBooksFromBooksWithRating.
+            return GetBooksFromBooksWithRating(topBooks);
         }
         //Dodati komentare u kod radi bolje čitljivosti i održavanja koda.
+
+        //Računa prosjeke ratinga za sve knjige i vraća listu objekata BookWithAverageRating
         public List<BookWithAverageRating> CalculateAverageRatings(List<Book> books)
         {
-            var booksWithAverageRatings = new List<BookWithAverageRating>(); //Dio koda treba izdvojiti iz metode GetBestBooks i staviti u zasebnu metodu GetBooksFromBooksWithRating.
+            var booksWithAverageRatings = new List<BookWithAverageRating>(); 
 
             foreach (var book in books)
             {
@@ -84,7 +86,17 @@ namespace VVS_projekat.Controllers
             return booksWithAverageRatings;
         }
 
+        //Pretvara listu objekata tipa BookWithAverageRating u listu Knjiga
+        private List<Book> GetBooksFromBooksWithRating(List<BookWithAverageRating> booksWithRating)
+        {
+            var books = new List<Book>();
+            foreach (var bookWithRating in booksWithRating)
+            {
+                books.Add(bookWithRating.Book);
 
+            }
+            return books;
+        }
 
         //Objekat koji sadrži knjige, i odgovarajući prosječan rating za svaku
         public class BookWithAverageRating
